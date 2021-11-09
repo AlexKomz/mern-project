061e4a683faea99162cfb87d2880b7e3389ad565
KVC\komzoltcev <komzoltcev.a@gmail.com>
2021-11-09 15:38:17 +0300

    Проект закончен

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,8 +1,11 @@
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import {useHttp} from "../hooks/http.hook";
 import {useMessage} from "../hooks/message.hook";
+import {AuthContext} from "../context/AuthContext";
 
 export const AuthPage = () => {
+  const auth = useContext(AuthContext);
+
   const message = useMessage();
 
   const {loading, error, request, clearError} = useHttp();
@@ -10,31 +13,35 @@ export const AuthPage = () => {
   const [form, setForm] = useState({
     email: ``, password: ``
   });
-
-  useEffect(() => {
-    message(error);
-    clearError();
-  }, [error, message, clearError]);
-
   const changeHandler = event => {
+
     setForm({...form, [event.target.name]: event.target.value});
   };
-
   const registerHandler = async () => {
+
     try {
       const data = await request(`/api/auth/register`, `POST`, {...form});
       message(data.message);
     } catch {
     }
   };
-
   const loginHandler = async () => {
+
     try {
       const data = await request(`/api/auth/login`, `POST`, {...form});
-      message(data.message);
+      auth.login(data.token, data.userId);
     } catch {
     }
   };
+
+  useEffect(() => {
+    message(error);
+    clearError();
+  }, [error, message, clearError]);
+
+  useEffect(() => {
+    window.M.updateTextFields();
+  }, []);
 
   return (
       <div className="row">
@@ -51,6 +58,7 @@ export const AuthPage = () => {
                       id="email" type="text"
                       name="email"
                       className="yellow-input"
+                      value={form.email}
                       onChange={changeHandler}
                   />
                   <label htmlFor="email">Email</label>
@@ -63,6 +71,7 @@ export const AuthPage = () => {
                       type="password"
                       name="password"
                       className="yellow-input"
+                      value={form.password}
                       onChange={changeHandler}
                   />
                   <label htmlFor="password">Пароль</label>
